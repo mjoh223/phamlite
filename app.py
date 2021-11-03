b4f72593be8fcac667561f76db829691afe1bfc4
mjoh223 <matthew.johnson@ucsf.edu>
2021-11-03 14:42:12 -0700

    changed mkdir to makedirs

--- app.py
+++ app.py
@@ -445,8 +445,8 @@ def load_dropdown(list_of_contents, list_of_names, list_of_dates):
     def randomString(stringLength=8):
         letters = string.ascii_lowercase
         return ''.join(random.choice(letters) for i in range(stringLength))
-    storage_path = '/app/phamlite_storage'
-    os.mkdir(storage_path)
+    storage_path = '/app/phamlite_storage/'
+    os.makedirs(storage_path, exist_ok=True)
     if list_of_contents is not None:
         f  =  os.path.join(storage_path, randomString(8))
         print(glob.glob('/app/.apt/*'), flush=True)
